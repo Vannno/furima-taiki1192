3ba7dca319b38fa7a9d9b637c67e3db6e77ae921
Van <taikin.banno@gmail.com>
2020-11-16 16:53:36 +0900

    商品消去機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit]
   
   def index  
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
   def edit
   end
 
-  def show
+  def destroy
   end
 
   def update
@@ -33,6 +33,12 @@ class ItemsController < ApplicationController
       redirect_to action: :show
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
     end
   end
 
